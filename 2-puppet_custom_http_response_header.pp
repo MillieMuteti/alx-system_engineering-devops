a25e3739b4308b0a013503eed9639dcce1945146
MillieMuteti <milliemuteti@gmail.com>
2022-12-19 11:43:35 -0800

    task 2

--- 2-puppet_custom_http_response_header.pp
+++ 2-puppet_custom_http_response_header.pp
@@ -1,25 +1,19 @@
-# Installs a Nginx server with custome HTTP header
-
-exec {'update':
-  provider => shell,
-    command  => 'sudo apt-get -y update',
-      before   => Exec['install Nginx'],
-      }
-
-exec {'install Nginx':
-  provider => shell,
-    command  => 'sudo apt-get -y install nginx',
-      before   => Exec['add_header'],
-      }
-
-exec { 'add_header':
-  provider    => shell,
-    environment => ["HOST=${hostname}"],
-      command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-        before      => Exec['restart Nginx'],
-	}
-
-exec { 'restart Nginx':
-  provider => shell,
-    command  => 'sudo service nginx restart',
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+    provider => shell,
     }
+    -> package {'nginx':
+      ensure => present,
+      }
+      -> file_line { 'header line':
+        ensure => present,
+	  path   => '/etc/nginx/sites-available/default',
+	    line   => "	location / {
+  add_header X-Served-By ${hostname};",
+    match  => '^\tlocation / {',
+    }
+    -> exec { 'restart service':
+      command  => 'sudo service nginx restart',
+        provider => shell,
+	}
